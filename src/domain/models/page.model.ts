bf5dcc2f2f6cd9fd09ff49fb5101b118ea1598ee
bielsolosos <bielrochasantoscoutinho@gmail.com>
2025-06-09 23:53:31 -0300

    refactor: removendo a model daqui

--- src/domain/models/page.model.ts
+++ src/domain/models/page.model.ts
@@ -15,12 +15,3 @@ export interface PageSummaryDto {
   title: string;
   updatedAt: Date;
 }
-
-export type Page = {
-  id: string;
-  title: string;
-  content: string;
-  createdAt: Date;
-  updatedAt: Date;
-  archived: boolean;
-};
