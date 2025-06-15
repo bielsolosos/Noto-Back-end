beaf586797b4d5ef72755863b4cfb794d1be2667
bielsolosos <bielrochasantoscoutinho@gmail.com>
2025-06-14 21:11:33 -0300

    feat: Adicionando endpoint de rota nn encontrada

--- src/server.ts
+++ src/server.ts
@@ -28,6 +28,10 @@ app.use("/", homeRoutes);
 app.use("/pages", pagesRoutes);
 app.use("/users", userRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Rota não encontrada" });
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando em ${ADDRESS}:${PORT}`);
   console.log(`Documentação da API em ${ADDRESS}:${PORT}/api-docs`);
