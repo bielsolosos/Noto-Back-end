1a4488066db2cae1dba1ce91732970717a18f427
Gabriel Coutinho <125703161+bielsolosos@users.noreply.github.com>
2026-03-19 18:21:08 -0300

    feat: Add unit tests UserServiceTest

--- src/test/java/space/bielsolososdev/noto/domain/users/service/UserServiceTest.java
+++ src/test/java/space/bielsolososdev/noto/domain/users/service/UserServiceTest.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import space.bielsolososdev.noto.core.exception.BusinessException;
 import space.bielsolososdev.noto.domain.users.model.Role;
 import space.bielsolososdev.noto.domain.users.model.User;
 import space.bielsolososdev.noto.domain.users.repository.RoleRepository;
@@ -48,10 +49,7 @@ class UserServiceTest {
 
     @BeforeEach
     void setUp() {
-        Role roleUser = new Role();
-        roleUser.setId(UUID.randomUUID());
-        roleUser.setName("ROLE_USER");
-        roleUser.setDescription("Usuário comum");
+        Role roleUser = getRoleUser();
 
         Role roleAdmin = new Role();
         roleAdmin.setId(UUID.randomUUID());
@@ -64,19 +62,27 @@ class UserServiceTest {
 
     }
 
+    Role getRoleUser(){
+        Role roleUser = new Role();
+        roleUser.setId(UUID.randomUUID());
+        roleUser.setName("ROLE_USER");
+        roleUser.setDescription("Usuário comum");
+        return roleUser;
+    }
+
     @Test
     void getMe() {
         SecurityContext securityContext = mock(SecurityContext.class);
         Authentication authentication = mock(Authentication.class);
-        authentication.setAuthenticated(true);
 
         // Abrir o bloco para métodos estáticos
         try (MockedStatic<SecurityContextHolder> mockedSecurity = mockStatic(SecurityContextHolder.class)) {
 
             mockedSecurity.when(SecurityContextHolder::getContext).thenReturn(securityContext);
-            //TODO falta mockar como Autenticado. Colocar o true
             when(securityContext.getAuthentication()).thenReturn(authentication);
+            when(authentication.isAuthenticated()).thenReturn(true);
             when(authentication.getName()).thenReturn(user.getUsername());
+
             when(repository.findByUsername(user.getUsername())).thenReturn(Optional.of(user));
 
             User me = userService.getMe();
@@ -86,11 +92,101 @@ class UserServiceTest {
     }
 
     @Test
-    void changePassword() {
+    void getMeNotAuthenticated() {
+        SecurityContext securityContext = mock(SecurityContext.class);
+        Authentication authentication = mock(Authentication.class);
+
+        // Abrir o bloco para métodos estáticos
+        try (MockedStatic<SecurityContextHolder> mockedSecurity = mockStatic(SecurityContextHolder.class)) {
+
+            mockedSecurity.when(SecurityContextHolder::getContext).thenReturn(securityContext);
+            when(securityContext.getAuthentication()).thenReturn(authentication);
+            // Mock não autenticado.
+            when(authentication.isAuthenticated()).thenReturn(false);
+
+            assertThrows(BusinessException.class, () -> userService.getMe());
+        }
     }
 
     @Test
-    void createUser() {
+    void getMeNotFound() {
+        SecurityContext securityContext = mock(SecurityContext.class);
+        Authentication authentication = mock(Authentication.class);
+
+        // Abrir o bloco para métodos estáticos
+        try (MockedStatic<SecurityContextHolder> mockedSecurity = mockStatic(SecurityContextHolder.class)) {
+
+            mockedSecurity.when(SecurityContextHolder::getContext).thenReturn(securityContext);
+            when(securityContext.getAuthentication()).thenReturn(authentication);
+            // Mock não autenticado.
+            when(authentication.isAuthenticated()).thenReturn(true);
+
+            //Mock do userName
+            when(repository.findByUsername(any())).thenReturn(Optional.empty());
+
+            assertThrows(BusinessException.class, () -> userService.getMe());
+        }
+    }
+
+    @Test
+    void changePasswordPasswordSuccess() {
+
+        when(repository.findById(user.getId())).thenReturn(Optional.of(user));
+        when(repository.save(user)).thenAnswer(invocation -> invocation.getArgument(0));
+
+        when(passwordEncoder.matches(any(), any())).thenReturn(true);
+        when(passwordEncoder.encode(any())).thenReturn("senhaMock");
+
+        User result = userService.changePassword(user.getId(), user.getPassword(), "senhaMock");
+
+        verify(repository, times(1)).findById(user.getId());
+        verify(passwordEncoder, times(1)).encode(any());
+        verify(passwordEncoder, times(1)).matches(any(), any());
+        assertNotNull(result);
+        assertEquals("senhaMock", result.getPassword());
+    }
+
+    @Test
+    void changePasswordPasswordIncorrect() {
+        when(repository.findById(user.getId())).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches(any(), any())).thenReturn(false);
+
+        assertThrows(BusinessException.class, () -> userService.changePassword(user.getId(), user.getPassword(), "senhaMock"));
+    }
+
+    @Test
+    void changePasswordPasswordUserNotFound() {
+        when(repository.findById(user.getId())).thenReturn(Optional.empty());
+        assertThrows(BusinessException.class, () -> userService.changePassword(user.getId(), user.getPassword(), "senhaMock"));
+    }
+
+    @Test
+    void createUserThrowsBusinessExceptionUsername() {
+        when(repository.findByUsername("bielsolosos")).thenReturn(Optional.of(user));
+        assertThrows(BusinessException.class, () -> userService.createUser("bielsolosos", "emailteste@teste.teste-bolado-mesmo", "****"));
+    }
+
+    @Test
+    void createUserThrowsBusinessExceptionEmail() {
+        when(repository.findByUsername("bielsolosos")).thenReturn(Optional.empty());
+        when(repository.findByEmail("primary-email@gmail.com")).thenReturn(Optional.of(user));
+
+        assertThrows(BusinessException.class, () -> userService.createUser("bielsolosos", "primary-email@gmail.com", "****"));
+    }
+
+    @Test
+    void createUserSuccess() {
+        when(repository.findByUsername(user.getUsername())).thenReturn(Optional.empty());
+        when(repository.findByEmail("primary-email@gmail.com")).thenReturn(Optional.empty());
+        when(repository.save(any())).thenAnswer(invocation -> invocation.getArgument(0));
+        when(roleRepository.findByName("ROLE_USER")).thenReturn(Optional.of(getRoleUser()));
+        when(passwordEncoder.encode(any())).thenReturn("senhaMock");
+
+        userService.createUser(user.getUsername(), user.getEmail(), user.getPassword());
+
+        verify(repository, times(1)).save(any());
+        assertNotNull(user);
+        assertNotNull(user.getId());
     }
 
     @Test
