e2192defa0dda98502e3559c2d55500474586206
Gabriel Coutinho <125703161+bielsolosos@users.noreply.github.com>
2026-06-03 17:36:10 -0300

    feat: Adicionando rate limit e adicionando endpoint de teste

--- src/main/java/br/dev/bielsolosos/noto/api/controller/rest/AuthRestController.java
+++ src/main/java/br/dev/bielsolosos/noto/api/controller/rest/AuthRestController.java
@@ -1,6 +1,8 @@
 package br.dev.bielsolosos.noto.api.controller.rest;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import br.dev.bielsolosos.noto.domain.users.model.dto.LoginRequest;
@@ -16,6 +18,7 @@ public class AuthRestController {
     private final AuthService service;
 
     @PostMapping("/login")
+    @RateLimiter(name = "public-routes", fallbackMethod = "mensagemMaluca")
     public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest request) {
         return ResponseEntity.ok(service.login(request));
     }
@@ -23,6 +26,11 @@ public class AuthRestController {
     @PostMapping("/refresh")
     public ResponseEntity<TokenResponse> refresh(@RequestBody RefreshRequest request) {
         return ResponseEntity.ok(service.refresh(request));
+    }
+
+    @SuppressWarnings("unused")
+    public ResponseEntity<TokenResponse> mensagemMaluca(LoginRequest request, Throwable throwable) {
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
     }
 }
 
